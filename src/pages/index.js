cb3c4c46432fa1164a6c3576d087172571ddaadf
anggihseptiawan <albertanggih@gmail.com>
2021-01-12 00:23:54 +0700

    make homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,86 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
-import { Button } from "@chakra-ui/react";
+import {
+	Flex,
+	Link,
+	Box,
+	Stack,
+	Avatar,
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	Heading,
+	Text,
+	Button,
+} from "@chakra-ui/react";
 
 export default function Home() {
 	return (
-		<div className={styles.container}>
+		<>
 			<Head>
-				<title>Create Next App</title>
+				<title>Better Comments</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main className={styles.main}>
-				<h1 className={styles.title}>
-					Welcome to <a href="https://nextjs.org">Next.js!</a>
-				</h1>
-
-				<p className={styles.description}>
-					Get started by editing{" "}
-					<code className={styles.code}>pages/index.js</code>
-				</p>
-
-				<Button>Login with github</Button>
+			<main>
+				<Flex flexDirection="column">
+					<Flex
+						justifyContent="space-between"
+						pt={3}
+						pb={3}
+						pl={10}
+						pr={10}
+					>
+						<Box display="flex" alignItems="center">
+							<Stack spacing={4} isInline>
+								<Link fontWeight="bold">Better Comments</Link>
+								<Link>Sites</Link>
+								<Link>Feedback</Link>
+							</Stack>
+						</Box>
+						<Box>
+							<Flex alignItems="center">
+								<Link mr={2}>Username</Link>
+								<Avatar size="sm" />
+							</Flex>
+						</Box>
+					</Flex>
+					<Flex />
+					<Flex
+						justifyContent="center"
+						backgroundColor="#eef6f7"
+						pt={4}
+						pb={4}
+					>
+						<Flex backgroundColor="#ffffff" p={8}>
+							<Breadcrumb>
+								<BreadcrumbItem>
+									<BreadcrumbLink>Sites</BreadcrumbLink>
+								</BreadcrumbItem>
+								<Heading>My Sites</Heading>
+								<Box
+									display="flex"
+									flexDirection="column"
+									alignItems="center"
+									mt={8}
+								>
+									<Heading as="h5" size="md">
+										Get comments better with better comments
+									</Heading>
+									<Text>Start today, then grow with us</Text>
+									<Button
+										variant="solid"
+										size="md"
+										mt={3}
+										backgroundColor="gray.200"
+									>
+										Get Started
+									</Button>
+								</Box>
+							</Breadcrumb>
+						</Flex>
+					</Flex>
+				</Flex>
 			</main>
-		</div>
+		</>
 	);
 }
